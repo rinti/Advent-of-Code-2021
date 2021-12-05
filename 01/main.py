c95442f4cfff68c9d8683a5cdf039f3e78dc9bdf
Andreas Bernacca <hello@rinti.se>
2021-12-05 12:00:21 +0100

    Add solution for part 2

--- 01/main.py
+++ 01/main.py
@@ -1,8 +1,21 @@
-from itertools import pairwise
+from itertools import pairwise, tee
 
-with open("./input.txt") as f:
+
+def part_one_solution(lines):
+    total = len([1 for curr, next in pairwise(lines) if int(curr) < int(next)])
+    print(total)
+
+
+def part_two_solution(lines):
+    lines = [int(x) for x in lines]
     i = 0
-    for curr, next in pairwise(f.readlines()):
-        if int(curr) < int(next):
+    for x in range(len(lines)):
+        if sum(lines[x : x + 3]) < sum(lines[x + 1 : x + 4]):
             i += 1
     print(i)
+
+
+with open("./input.txt") as f:
+    lines = f.readlines()
+    part_one_solution(lines)
+    part_two_solution(lines)
