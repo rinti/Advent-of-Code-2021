76723feb023ff82c5c6f7ed6b4919a53ec1eb0f1
Andreas Bernacca <hello@rinti.se>
2021-12-05 13:54:47 +0100

    Add part two solution

--- 02/main.py
+++ 02/main.py
@@ -1,12 +1,31 @@
 from collections import defaultdict
 
-with open("./input.txt") as f:
-    dict = defaultdict(int)
-    for line in f.readlines():
-        dir, amount = line.split(" ")
-        dict[dir] += int(amount)
 
+def part_one_solution(dict):
     forward = dict["forward"]
     depth = dict["down"] - dict["up"]
 
     print(forward * depth)
+
+
+def part_two_solution(dict):
+    print(abs(dict["forward"] * dict["depth"]))
+
+
+with open("./input.txt") as f:
+    dict = defaultdict(int)
+    dict["aim"] = 0
+    dict["depth"] = 0
+
+    for line in f.readlines():
+        dir, amount = line.split(" ")
+        dict[dir] += int(amount)
+        if dir in ["down", "up"]:
+            dict["aim"] = (
+                dict["aim"] + int(amount) if dir == "up" else dict["aim"] - int(amount)
+            )
+        if dir == "forward":
+            dict["depth"] += dict["aim"] * int(amount)
+
+    part_one_solution(dict)
+    part_two_solution(dict)
